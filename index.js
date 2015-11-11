ef734faee557a269fa96a1af3b732a851ca982fe
Jose Antonio Moreno Punzon <joseanmp@gmail.com>
2015-11-11 02:58:31 +0100

    port

--- index.js
+++ index.js
@@ -27,5 +27,5 @@ require('./routes')(app);
 // This file has been called directly with 
 // `node index.js`. Start the server!
 
-app.listen(8080);
+app.listen(5000);
 console.log('Your application is running on http://localhost:8080');
